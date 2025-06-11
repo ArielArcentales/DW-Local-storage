d170a01ab0cdb45573a4fedccc1f981e874ada9d
Estudiante Ltic <estudiante@sistemas.local>
2025-06-10 23:06:58 -0500

    Primeras 3 funcionalidades aplicadas

--- script.js
+++ script.js
@@ -4,11 +4,24 @@ let lista = document.getElementById("lista-contactos");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    let contact = {
-        nombre: document.getElementById("nombre").value,
-        correo: document.getElementById("correo").value,
-        telefono: document.getElementById("telefono").value,
-    };
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = document.getElementById("telefono").value;
+
+    if (nombre.length < 2) {
+        alert("El nombre debe tener al menos 2 caracteres.");
+        return;
+    }
+
+    for (let char of telefono) {
+    if (char < '0' || char > '9') {
+        alert("El teléfono solo debe contener números.");
+        return;
+    }
+}
+
+
+    let contact = { nombre, correo, telefono };
 
     let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
     contactos.push(contact);
@@ -20,6 +33,8 @@ form.addEventListener("submit", (e) => {
 
 function mostrarContactos() {
     let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
+
+    contactos.sort((a, b) => a.nombre.localeCompare(b.nombre));
     lista.innerHTML = "";
 
     contactos.forEach((c, i) => {
@@ -35,15 +50,17 @@ function mostrarContactos() {
 
 function eliminarContacto(index) {
     let contactos = JSON.parse(localStorage.getItem("contactos")) || [];
+    contactos.splice(index);
     localStorage.setItem("contactos", JSON.stringify(contactos));
     mostrarContactos(); 
 }
+
 
 mostrarContactos();
 
 
 //elimianar contactos
 //ordenar los contactos alfabeticamente mediante el nombre
-//añadir validaciones: nombre - telefono 
+//añadir validaciones: nombre (minimo 2 caracteres)- telefono (no se admiten letras)  
 //editar un contacto
 //mostrar de 5, 10 o 15 contactos
